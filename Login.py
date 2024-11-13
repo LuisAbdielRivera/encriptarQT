587b7f6a5e8b6eee31d59026761e0413c9df0aeb
LuisAbdielRivera <147383444+LuisAbdielRivera@users.noreply.github.com>
2024-11-13 00:41:38 -0600

    update

--- Login.py
+++ Login.py
@@ -8,7 +8,7 @@ class MainWindow(QtWidgets.QMainWindow,Ui_MainWindow):
 
         self.label_2.setText("Hola Bienvenido a mi App")
         self.btnAceptar.clicked.connect(self.validar)
-        self.btnCancelar.clicked.connect(self.validar)
+        self.btnCancelar.clicked.connect(self.salir)
 
     def validar(self):
         usuario = self.txtUsuario.text()
@@ -20,6 +20,16 @@ class MainWindow(QtWidgets.QMainWindow,Ui_MainWindow):
             self.hide()
         else:
             self.label_2.setText("Acceso Denegado")
+    
+    def salir(self):
+        msgBox = QMessageBox()
+        msgBox.setIcon(QMessageBox.Critical)
+        msgBox.setText("Error de usuario o contraseña")
+        msgBox.setWindowTitle("Error")
+        msgBox.setStandardButtons(QMenssageBox.Ok | QMessageBox.Cancel)
+        returnvValue = msgBox.exec()
+        if returnvValue == QMenssageBox.Ok:
+            self.close()
     
     def openMenu(self):
         opennewwindow = Menu(self)
