74785d173ca2c40c2b6e19202eff8be0cf25033e
LuisAbdielRivera <147383444+LuisAbdielRivera@users.noreply.github.com>
2024-11-12 18:26:10 -0600

    update

--- Login.py
+++ Login.py
@@ -1,21 +1,29 @@
 from FrmLogin import *
+from Menu import *
 
 class MainWindow(QtWidgets.QMainWindow,Ui_MainWindow):
     def __init__(self, *args, **kwargs):
         QtWidgets.QMainWindow.__init__(self, *args, **kwargs)
         self.setupUi(self)
 
-        self.label_2.setText("Hola Mundo")
+        self.label_2.setText("Hola Bienvenido a mi App")
         self.btnAceptar.clicked.connect(self.validar)
+        self.btnCancelar.clicked.connect(self.validar)
 
     def validar(self):
         usuario = self.txtUsuario.text()
         contrasena = self.txtContrasena.text()
 
         if usuario == "Abdiel" and contrasena == "1234":
-            print("La contraseña y el usuario son correctos")
+            self.label_2.setText("Acceso Consedido")
+            self.openMenu()
+            self.hide()
         else:
-            print("El usuario o la contraseña son incorrectos")
+            self.label_2.setText("Acceso Denegado")
+    
+    def openMenu(self):
+        opennewwindow = Menu(self)
+        opennewwindow.show()
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication([])
---
+++ Menu.py
@@ -0,0 +1,6 @@
+from FrmMenu import *
+
+class Menu(QtWidgets.QMainWindow, Ui_FrmMenu):
+    def __init__(self, *args, **kwars ):
+        QtWidgets.QMainWindow.__init__(self, *args, **kwars)
+        self.setupUi(self)
