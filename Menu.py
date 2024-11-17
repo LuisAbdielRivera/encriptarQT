baad09bf42f4bf0cdd1ad3a1f4358240f76ae01d
LuisAbdielRivera <147383444+LuisAbdielRivera@users.noreply.github.com>
2024-11-16 23:50:42 -0600

    update

--- Menu.py
+++ Menu.py
@@ -1,14 +1,17 @@
 from views.FrmMenu import *
 from Encriptar import *
 from Desencriptar import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
 
 class Menu(QtWidgets.QMainWindow, Ui_FrmMenu):
     def __init__(self, *args, **kwars ):
         QtWidgets.QMainWindow.__init__(self, *args, **kwars)
         self.setupUi(self)
 
-        self.btnEncriptar.clicked.connect(self.openEncriptar)
-        self.btnDesencriptar.clicked.connect(self.openDesencriptar)
+        self.encriptarNuevo.triggered.connect(self.openEncriptar)
+        self.desencriptarNuevo.triggered.connect(self.openDesencriptar)
 
     def openEncriptar(self):
         opennewwindow = Encriptar(self)
