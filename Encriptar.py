81686261529428147ab11ec86c8f77b205d77457
LuisAbdielRivera <147383444+LuisAbdielRivera@users.noreply.github.com>
2024-11-19 13:49:41 -0600

    update

--- Encriptar.py
+++ Encriptar.py
@@ -4,3 +4,11 @@ class Encriptar(QtWidgets.QMainWindow, Ui_FrmEncriptar):
     def __init__(self, *args, **kwars ):
         QtWidgets.QMainWindow.__init__(self, *args, **kwars)
         self.setupUi(self)
+
+        self.btnEncriptar.clicked.connect(self.encriptarAES)
+
+    def encriptarAES(self):
+        data = self.txtMensaje.toPlainText()
+        key = b"123456789101112131415161718_UTXJ"
+        iv = b"TI_UTXJ2024IGDS"
+        print(data)
