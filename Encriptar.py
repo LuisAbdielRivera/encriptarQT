9116e8af308782c2ee456521cac0083bfe0b6c94
LuisAbdielRivera <147383444+LuisAbdielRivera@users.noreply.github.com>
2024-11-21 15:07:16 -0600

    update

--- Encriptar.py
+++ Encriptar.py
@@ -11,6 +11,8 @@ class Encriptar(QtWidgets.QMainWindow, Ui_FrmEncriptar):
 
         self.btnEncriptar.clicked.connect(self.encriptarAES)
         self.btnLimpiar.clicked.connect(self.limpiarCampos)
+        self.btnGuardar.clicked.connect(self.guardarDatos)
+        self.btnCargar.clicked.connect(self.cargarMensaje)
 
     def encriptarAES(self):
         data = self.txtMensaje.toPlainText()
@@ -29,3 +31,37 @@ class Encriptar(QtWidgets.QMainWindow, Ui_FrmEncriptar):
     def limpiarCampos(self):
         self.lblMensajeEncriptado.clear()
         self.txtMensaje.clear()
+
+    def guardarDatos(self):
+        mensaje_original = self.txtMensaje.toPlainText()
+        mensaje_encriptado = self.lblMensajeEncriptado.text()
+
+        with open("C:\Prueba\datos_guardados.txt", "w", encoding="utf-8") as archivo:
+            archivo.write("Mensaje Original:\n")
+            archivo.write(mensaje_original + "\n\n")
+            archivo.write("Mensaje Encriptado:\n")
+            archivo.write(mensaje_encriptado + "\n")
+
+        print("Datos PA")
+
+    def cargarMensaje(self):
+        try:
+            with open("C:\Prueba\datos_guardados.txt", "r", encoding="utf-8") as archivo:
+                contenido = archivo.readlines()
+
+            mensaje_original = ""
+            mensaje_encriptado = ""
+
+            for i, linea in enumerate(contenido):
+                if linea.strip() == "Mensaje Original:":
+                    mensaje_original = contenido[i + 1].strip()
+                elif linea.strip() == "Mensaje Encriptado:":
+                    mensaje_encriptado = contenido[i + 1].strip()
+
+            self.txtMensaje.setPlainText(mensaje_original)
+            self.lblMensajeEncriptado.setText(mensaje_encriptado)
+            print("Archivo cargado exitosamente.")
+        except FileNotFoundError:
+            print("El archivo no existe. Asegúrate de haber guardado los datos primero.")
+        except Exception as e:
+            print(f"Ocurrió un error al cargar el archivo: {e}")
